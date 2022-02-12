e3de3105abf53eb575ae680ced7f7cd09e71cd2e
Adam Reis <adam@reis.nz>
2022-02-13 12:03:51 +1300

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
-# joi-luxon
+# @helloclub/joi-luxon
+
+[![npm version](https://img.shields.io/npm/v/@helloclub/joi-luxon.svg)](https://www.npmjs.com/package/@helloclub/joi-luxon)
+[![github issues](https://img.shields.io/github/issues/helloclub/joi-luxon.svg)](https://github.com/helloclub/joi-luxon/issues)
+
+
 A Joi extension to automatically convert ISO date strings to Luxon DateTime objects
+
+![Hello Club](https://helloclub.com/images/logo/logo-text.svg)
+
+### Features
+- Validates incoming date values as ISO date strings
+- Automatically converts date strings to Luxon `DateTime` objects
+- Able to specify timezone and use modifiers like `startOf` or `endOf`
+- Able to validate using functions like `isAfter` or `isBefore`
+
+### Installation
+
+```shell
+#npm
+npm install @helloclub/joi-luxon
+
+#yarn
+yarn add @helloclub/joi-luxon
+```
+
+### Usage
+```js
+const Joi = require('@hapi/joi')
+const JoiLuxon = require('@helloclub/joi-luxon')
+
+module.exports = Joi.extend(JoiLuxon)
+```
+
+### Examples
+
+Validate a start and end date, setting the date to start and end of day respectively and validating that the end date is after the start date:
+
+```js
+const schema = Joi.object({
+  startDate: Joi
+    .luxon()
+    .setZone(timezone)
+    .startOf('day'),
+  endDate: Joi
+    .luxon()
+    .setZone(timezone)
+    .endOf('day')
+    .gt(Joi.ref('startDate')),
+})
+```
+
+Set the value to a given minimum date if it’s less than that:
+
+```js
+const schema = Joi.object({
+  date: Joi
+    .luxon()
+    .required()
+    .min(Joi.ref('$minDate'))
+})
+
+const schema = Joi.object({
+  date: Joi
+    .luxon()
+    .required()
+    .min(new Date())
+})
+```
+
+All available rules and helpers:
+
+```js
+Joi
+
+  //Base validation and conversion to Luxon DateTime object
+  .luxon()
+
+  //Sets the timezone on the object
+  .setZone(timezone)
+
+  //Modified the DateTime instance to the start/end of the given period
+  .startOf(period)
+  .endOf(period)
+
+  //Validates silently against a minimum / maximum date
+  .min(date)
+  .max(date)
+
+  //Validate and throw errors if the date is less than or greater than a reference date
+  .lt(date)
+  .gt(date)
+  .lte(date)
+  .gte(date)
+```
+
+## Issues & feature requests
+
+Please report any bugs, issues, suggestions and feature requests in the [joi-luxon issue tracker](https://github.com/helloclub/joi-luxon/issues).
+
+## Sponsor
+
+This package is sponsored by [Hello Club](https://helloclub.com?source=github), an [all-in-one club and membership management solution](https://helloclub.com?source=github) complete with booking system, automated membership renewals, online payments and integrated access and light control. 
+
+If you belong to any kind of club or membership based organisation, or if you know someone who helps run a club, please [check us out](https://helloclub.com?source=github)!
+
+## License
+
+(MIT License)
+
+Copyright 2022, Hello Club
---
+++ index.js
@@ -0,0 +1,247 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+const {DateTime} = require('luxon')
+
+/**
+ * Extend Joi with Luxon date handling
+ */
+module.exports = Joi => ({
+  type: 'luxon',
+  base: Joi.any(),
+  messages: {
+    'luxon.lt': `must be before {{#date}}"`,
+    'luxon.gt': `must be after {{#date}}"`,
+    'luxon.lte': `must be same as or before {{#date}}"`,
+    'luxon.gte': `must be same as or after {{#date}}"`,
+  },
+  coerce(value, {schema, state, prefs}) {
+
+    //No value
+    if (!value) {
+      return
+    }
+
+    //Convert to DateTime object
+    if (!(value instanceof DateTime)) {
+      value = DateTime.fromISO(value)
+    }
+
+    //If invalid at this stage, return as is
+    if (!value.isValid) {
+      return
+    }
+
+    //Get flags
+    const timezone = schema.$_getFlag('timezone')
+    const startOf = schema.$_getFlag('startOf')
+    const endOf = schema.$_getFlag('endOf')
+    let max = schema.$_getFlag('max')
+    let min = schema.$_getFlag('min')
+
+    //Resolve references
+    if (Joi.isRef(min)) {
+      min = min.resolve(value, state, prefs)
+    }
+    if (Joi.isRef(max)) {
+      max = max.resolve(value, state, prefs)
+    }
+
+    //Apply a timezone
+    if (timezone) {
+      value.setZone(timezone)
+    }
+    else if (prefs.context.timezone) {
+      value.setZone(prefs.context.timezone)
+    }
+
+    //Start/end of period
+    if (startOf) {
+      value.startOf(startOf)
+    }
+    if (endOf) {
+      value.endOf(endOf)
+    }
+
+    //Min/max dates
+    if (min && value < min) {
+      value = min
+    }
+    if (max && value > max) {
+      value = max
+    }
+
+    //Return value
+    return {value}
+  },
+  validate(value, helpers) {
+
+    //No value
+    if (!value) {
+      return value
+    }
+
+    //Invalid date provided
+    if (!value.isValid) {
+      const errors = helpers.error('date.iso')
+      return {value, errors}
+    }
+  },
+  rules: {
+    setZone: {
+      method(timezone) {
+        return this.$_setFlag('timezone', timezone)
+      },
+    },
+    startOf: {
+      method(startOf) {
+        return this.$_setFlag('startOf', startOf)
+      },
+    },
+    endOf: {
+      method(endOf) {
+        return this.$_setFlag('endOf', endOf)
+      },
+    },
+    max: {
+      method(max) {
+        return this.$_setFlag('max', max)
+      },
+    },
+    min: {
+      method(min) {
+        return this.$_setFlag('min', min)
+      },
+    },
+    lt: {
+      method(date) {
+        return this.$_addRule({
+          name: 'lt',
+          args: {date},
+        })
+      },
+      args: [
+        {
+          name: 'date',
+          ref: true,
+          assert: (value) => (
+            !value || typeof value === 'string' || 
+            value instanceof Date || value instanceof DateTime
+          ),
+          message: 'must be a date string, Date, or DateTime object',
+        },
+      ],
+      validate(value, helpers, args) {
+        let {date} = args
+        if (!date) {
+          return value
+        }
+        if (typeof date === 'string') {
+          date = DateTime.fromISO(date)
+        }
+        if (!(value instanceof DateTime) || value < date) {
+          return value
+        }
+        return helpers.error('luxon.lt', {date})
+      },
+    },
+    gt: {
+      method(date) {
+        return this.$_addRule({
+          name: 'gt',
+          args: {date},
+        })
+      },
+      args: [
+        {
+          name: 'date',
+          ref: true,
+          assert: (value) => (
+            !value || typeof value === 'string' ||
+            value instanceof Date || value instanceof DateTime
+          ),
+          message: 'must be a date string, Date, or DateTime object',
+        },
+      ],
+      validate(value, helpers, args) {
+        let {date} = args
+        if (!date) {
+          return value
+        }
+        if (typeof date === 'string') {
+          date = DateTime.fromISO(date)
+        }
+        if (!(value instanceof DateTime) || value > date) {
+          return value
+        }
+        return helpers.error('luxon.gt', {date})
+      },
+    },
+    lte: {
+      method(date) {
+        return this.$_addRule({
+          name: 'lte',
+          args: {date},
+        })
+      },
+      args: [
+        {
+          name: 'date',
+          ref: true,
+          assert: (value) => (
+            !value || typeof value === 'string' ||
+            value instanceof Date || value instanceof DateTime
+          ),
+          message: 'must be a date string, Date, or DateTime object',
+        },
+      ],
+      validate(value, helpers, args) {
+        let {date} = args
+        if (!date) {
+          return value
+        }
+        if (typeof date === 'string') {
+          date = DateTime.fromISO(date)
+        }
+        if (!(value instanceof DateTime) || value <= date) {
+          return value
+        }
+        return helpers.error('luxon.lte', {date})
+      },
+    },
+    gte: {
+      method(date) {
+        return this.$_addRule({
+          name: 'gte',
+          args: {date},
+        })
+      },
+      args: [
+        {
+          name: 'date',
+          ref: true,
+          assert: (value) => (
+            !value || typeof value === 'string' ||
+            value instanceof Date || value instanceof DateTime
+          ),
+          message: 'must be a date string, Date, or DateTime object',
+        },
+      ],
+      validate(value, helpers, args) {
+        let {date} = args
+        if (!date) {
+          return value
+        }
+        if (typeof date === 'string') {
+          date = DateTime.fromISO(date)
+        }
+        if (!(value instanceof DateTime) || value >= date) {
+          return value
+        }
+        return helpers.error('luxon.gte', {date})
+      },
+    },
+  },
+})
